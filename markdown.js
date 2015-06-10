ccfc8dc4a40ec90d083baf7676ef36010740898b
Jeff Strinko <jeff@glip.com>
2015-06-10 15:16:38 -0400

    make inside url detection fast

--- markdown.js
+++ markdown.js
@@ -16,10 +16,10 @@ var Markdown = function(raw, options) {
 	var quote_tag = options.use_blockquote ? 'span class="blockquote"' : 'q';
 	var end_quote_tag = options.use_blockquote ? '/span' : '/q';
 	var bench = +new Date();
-	var last_offset = 0;
+	var link_last_offset = bold_last_offset = italic_last_offset = strike_last_offset = underline_last_offset = 0;
 	var test = 0;
 	var total = 0;
-	var was_inside_tag = false;
+	var link_was_inside_tag = bold_in_url = italic_in_url = strike_in_url = underline_in_url = false;
 	var val = raw.replace(/\&\#x2F;/g, '/'). // not sure why underscore replaces these...docs don't even claim that it does 
 		replace(/\[([^\]]*?)\]\(([\s\S]*?)\)/g, function(full_match, text, link) {
 			if (text === 'code') {
@@ -47,17 +47,17 @@ var Markdown = function(raw, options) {
 			full_str
 	) {
 			var sub_bench = +new Date();
-			var start = full_str.substr(last_offset, offset - last_offset);
-			last_offset = offset;
-			if (start.match(/[\s\S]*\($/)) {
+			var start = full_str.substr(link_last_offset, offset - link_last_offset);
+			link_last_offset = offset;
+			if (start.match(/.*\($/)) {
 				return full_match;
 			}
-			var inside_tag = was_inside_tag ? (start.match(/.*>[^<]*$/) ? false : true) : start.match(/.*<[^>]*$/);
+			var inside_tag = link_was_inside_tag ? (start.match(/.*>[^<]*$/) ? false : true) : start.match(/.*<[^>]*$/);
 			if (inside_tag) {
-				was_inside_tag = true;
+				link_was_inside_tag = true;
 				return full_match;
 			}
-			was_inside_tag = false;
+			link_was_inside_tag = false;
 			var inside_post_tag = start.match(/.*(\{\{-\{\{)(?![\s\S]*\}\}-\}\})[\s\S]*$/);
 			if (inside_post_tag) {
 				return full_match;
@@ -118,27 +118,43 @@ var Markdown = function(raw, options) {
 		}).
 		replace(/\*\*(\S[^\*\*]*?\S)\*\*/g, function(full_match, text, offset, full_str) {
 			total++;
-			if (Markdown.is_in_url(full_match, text, offset, full_str)) {
+			if (Markdown.is_in_url(full_match, text, offset, full_str, bold_in_url, bold_last_offset)) {
+				bold_last_offset = offset;
+				bold_in_url = true;
 				return full_match;
 			}
+			bold_last_offset = offset;
+			bold_in_url = false;
 			return "<b>" + text + "</b>";
 		}).
 		replace(/\*(\S[^\*]*?\S)\*/g, function(full_match, text, offset, full_str) {
-			if (Markdown.is_in_url(full_match, text, offset, full_str)) {
+			if (Markdown.is_in_url(full_match, text, offset, full_str, italic_in_url, italic_last_offset)) {
+				italic_last_offset = offset;
+				italic_in_url = true;
 				return full_match;
 			}
+			italic_last_offset = offset;
+			italic_in_url = false;
 			return "<i>" + text + "</i>";
 		}).
 		replace(/__(\S[^__]*?\S)__/g, function(full_match, text, offset, full_str) {
-			if (Markdown.is_in_url(full_match, text, offset, full_str)) {
+			if (Markdown.is_in_url(full_match, text, offset, full_str, underline_in_url, underline_last_offset)) {
+				underline_last_offset = offset;
+				underline_in_url = true;
 				return full_match;
 			}
+			underline_last_offset = offset;
+			underline_in_url = false;
 			return "<u>" + text + "</u>";
 		}).
 		replace(/~~(\S[^~~]*?\S)~~/g, function(full_match, text, offset, full_str) {
-			if (Markdown.is_in_url(full_match, text, offset, full_str)) {
+			if (Markdown.is_in_url(full_match, text, offset, full_str, strike_in_url, strike_last_offset)) {
+				strike_last_offset = offset;
+				strike_in_url = true;
 				return full_match;
 			}
+			strike_last_offset = offset;
+			strike_in_url = false;
 			return "<strike>" + text + "</strike>";
 		}).
 		replace(/\{\{\[\[-([^\{\{\[\[]*?)-\]\]\}\}/g, function(full_match, text, offset, full_str) {
@@ -153,9 +169,9 @@ var Markdown = function(raw, options) {
 	return val;
 };
 
-Markdown.is_in_url = function(full_match, text, offset, full_str) {
-	var start = full_str.substr(0, offset);
-	var inside_tag = start.match(/.*<[^>]*$/);
+Markdown.is_in_url = function(full_match, text, offset, full_str, already_in_url, last_offset) {
+	var start = full_str.substr(last_offset, offset - last_offset);
+	var inside_tag = already_in_url ? !start.match(/.*>[^<]*$/) : start.match(/.*<[^>]*$/);
 	if (inside_tag) { return true; }
 	var last_str = /([A-Za-z0-9\!\#\$\%\&\'\*\+\-\/\=\?\%\_\`\{\|\}\~\.\:]+)$/g.exec(start);
 	if (last_str) {
