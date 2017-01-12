99001e9b25ebe37f7ed88751a8ef958467331c1a
Jeff Strinko <jeff.strinko@ringcentral.com>
2017-01-12 15:23:55 -0500

    strip potential exploit

--- test/markdown.js
+++ test/markdown.js
@@ -4,6 +4,13 @@ var Markdown = require('../markdown').Markdown,
 
 var tests = [
 	{
+		str: "{{-{{img src=x onerror=prompt(document.cookie)}}-}}{{-{{/img}}-}}"
+		len: 45,
+		escaped_len: 45
+	},
+];
+var foo = [
+	{
 		str: "| **When** | 11:30am on Monday |\n| **Account** | dan@close.com |\n| **From** | Dave Varenos |\n| **To** | Dan Foody |",
 		len: 431,
 		escaped_len: 431
