aa989a6be4e0928e143544f6a7e88cc2127a9ae7
Ian Cole <ian.cole.dev@gmail.com>
2018-07-16 15:57:24 -0400

    Re-enable phone number parsing and improve regex

--- markdown.js
+++ markdown.js
@@ -24,8 +24,7 @@ if ((typeof require != 'undefined') && !_) {
 
 var Markdown = function(raw, options) {
 	var options = options || {};
-	// Disabling Until Clickable phone numbers is ready for production
-	// var phone_util;
+	var phone_util;
 
 	if (!raw) {
 		return '';
@@ -33,10 +32,9 @@ var Markdown = function(raw, options) {
 	if (!options.dont_escape) {
 		raw = _.escape(raw);
 	}
-	// Disabling Until Clickable phone numbers is ready for production
-	// if (typeof Client !== 'undefined') {
-	// 	phone_util = Client.get_controller('Phone_Number');
-	// }
+	if (typeof Client !== 'undefined') {
+		phone_util = Client.get_controller('Phone_Number');
+	}
 
 	var code_blocks = {};
 	var block_count = 0;
@@ -260,15 +258,14 @@ var Markdown = function(raw, options) {
 		})
 		.replace(/mailto:<a href=/g, function(full_match, which) {
 			return "<a href=";
-		});
-		// Disabling Until Clickable phone numbers is ready for production
-		// .replace(/\S*\d+\S*/g, function mark_phone_numbers(match) {
-		// 	if (!phone_util || !phone_util.is_valid_pstn(match)) {
-		// 		return match;
-		// 	}
+		})
+		.replace(/(?:\+)?(?:\d)?(?:\s|\.|-)?(?:\()?\d{3}(?:\))?(?:-|\s|.)?\d{3}(?:-|\s|\.)?\d{2}(?:\s|\.|-)?\d{2}/g, function mark_phone_numbers(match) {
+			if (!phone_util || !phone_util.is_valid_pstn(match)) {
+				return match;
+			}
 
-		// 	return "<a href='tel:" + match + "' class='markdown_phone_number'>" + match + '</a>';
-		// });
+			return "<a href='tel:" + match + "' class='markdown_phone_number'>" + match + '</a>';
+		});
 	return val;
 };
 
